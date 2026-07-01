dbf7c356b7623b5c237b374f8901b91a0a936d3d
Andrew Popravka <dioxyphthalophenone@tutamail.com>
2026-07-01 15:58:30 +0300

    Add files via upload

---
+++ software/web/Prefences panel/Blackout_Responce.php
@@ -0,0 +1,25 @@
+<?php
+$conn = mysqli_connect(
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME
+);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, state FROM b_o";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo json_encode($row);
+    }
+ } else {
+     echo '0 results';
+ }
+
+mysqli_close($conn);
+
+?>
